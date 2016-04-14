bae8ed641a29b5e20e1a9ba54c7f6b6d8e3a8272
alewitt <alewitt2@ufl.edu>
2016-04-14 03:35:45 -0400

    code cleanup

--- gather.php
+++ gather.php
@@ -1,33 +1,13 @@
 <?php
-require('/usr/local/www/hw2-creds.php');
+require('db_access.php');
 
-try {
-
-  $conn = new PDO('mysql:host=localhost;dbname=HW2', $db_user, $db_pass);
-  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-
-  function get_url($conn, $id){
-    $stmt = $conn->prepare('SELECT * FROM Joshes WHERE id= :id');
-    $stmt->execute(['id' => $id]);
-    if($row = $stmt->fetch()){
-      return $row['url'];
-    } else {
-      return 'No Match Found';
-    }
-  }
-
-  $image = 'nothing';
-  if(isset($_GET['joshMood'])){
-    $image = get_url($conn, $_GET['joshMood']);
-  }
-
-  $reply = [
-    'image' => $image,
-  ];
-
-  echo json_encode($reply);
-
-} catch (PDOException $e) {
-  echo 'ERROR ', $e->getMessage();
+$image = 'nothing';
+if(isset($_GET['joshMood'])){
+  $image = get_url($conn, $_GET['joshMood']);
 }
+
+$reply = [
+  'image' => $image,
+];
+
+echo json_encode($reply);
